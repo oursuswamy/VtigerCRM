c189ec4647832357d2ed213a9e9905b562cf0850
oursuswamy <126146908+oursuswamy@users.noreply.github.com>
2023-06-29 06:08:21 -0400

    Update Firstprogram.java

--- src/main/java/Automation/Firstprogram.java
+++ src/main/java/Automation/Firstprogram.java
@@ -4,6 +4,9 @@ public class Firstprogram {
 
 	public static void main(String[] args) {
 		System.out.println("my first program");
+		System.out.println("my first Automation project");
+
+
 		// TODO Auto-generated method stub
 
 	}
