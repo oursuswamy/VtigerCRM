4a86008a8270637fcd7db2d941ceeebf7e441cb6
DELL <DELL@DESKTOP-HU296J7>
2023-06-29 05:39:37 -0400

    Its my first project on GitHub

---
+++ src/main/java/Automation/Firstprogram.java
@@ -0,0 +1,11 @@
+package Automation;
+
+public class Firstprogram {
+
+	public static void main(String[] args) {
+		System.out.println("my first program");
+		// TODO Auto-generated method stub
+
+	}
+
+}
